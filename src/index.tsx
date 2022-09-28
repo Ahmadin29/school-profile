1af235d963ab1ac05d4890595ba0883e28aba330
Ahmad Saefudin <ahmaddin211056@gmail.com>
2022-09-29 00:10:29 +0700

    home

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import Text from "../components/Text";
+import Colors from "../constants/colors";
+
+export default function Index() {
+    return(
+        <View style={{
+            flex:1,
+            backgroundColor:Colors.white,
+        }} >
+            <Text>asd</Text>
+        </View>
+    )
+}
