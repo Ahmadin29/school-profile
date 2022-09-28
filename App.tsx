20b279fe5dc9ec099d2af6e65c150172a36eebd3
Ahmad Saefudin <ahmaddin211056@gmail.com>
2022-09-29 00:08:47 +0700

    basic config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import useCachedResources from './hooks/useCachedResources';
+import Navigators from './navigations';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const loadingResources = useCachedResources();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar translucent={false} backgroundColor="white" />
+      <Navigators/>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
